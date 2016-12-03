78bf9946d4398d4b8024042d4751df895866f03c
d-paller <darthdollar@gmail.com>
2016-12-03 17:11:53 -0500

    UI chnages.  Impliment background bluring.  Not done

---
+++ BellHouse/scripts/Custom.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $(".menu-tab-btn").click(function () {
+        window.scrollTo(0, 0);
+    });
+
+    $(window).on('scroll', function () {
+        var x = $(document).scrollTop();
+        x = x / 100;
+        $(".backdrop").css({ "-webkit-filter": "blur(" + x + "px)", "filter": "blur(" + x + "px)" })
+    })
+});
+
